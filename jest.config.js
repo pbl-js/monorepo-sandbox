bf986792bb14acd8012033f2a25427486a448a2e
Pawel Milczak <pawel.milczak@monogo.pl>
2021-07-13 23:42:52 +0200

    Create core lib

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/passit-admin', '<rootDir>/apps/passit-api'],
+  projects: ['<rootDir>/apps/passit-admin', '<rootDir>/apps/passit-api', '<rootDir>/libs/core'],
 }
